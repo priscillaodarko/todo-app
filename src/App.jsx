bf7c40df1d407a371815575d996acc3833b64193
Priscilla Okyere <priscillaodarko@gmail.com>
2023-03-20 22:56:05 +0000

    Added broswer router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.scss';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+
 
 function App() {
-  return 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      <Routes/>
+    <BrowserRouter/>  
+
+  )
   <>  
     <h1>Hello World!</h1>
 
